29e84c3bab449d9df7c30edf36eb834498cca314
codev009 <antoney007i@gmail.com>
2022-11-01 16:02:46 +0530

    add gruvbox theme + lualine gruvbox

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require('codev009.base')
 require('codev009.highlights')
 require('codev009.maps')
 require('codev009.plugins')
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
